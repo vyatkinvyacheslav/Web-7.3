07df0403a03af7426551676ea7833e5f4cf84d56
Vyacheslav Vyatkin <Vyatkin.vyacheslav.a@yandex.ru>
2024-06-24 18:17:42 +0500

    commit 24.06

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://petstore.swagger.io/v2/",
+    projectId: "obo43h",
     
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/zoo.cy.js
+++ cypress/e2e/zoo.cy.js
@@ -1,40 +1,88 @@
-describe('zoo api test', () => {
-  const baseUrl = `https://petstore.swagger.io/v2`;
-  it('Создание пользователя', () => {
-    cy.request({
-      method: `POST`,
-      url: `${baseUrl}/user`,
-      body: {
-        "id": 1,
-        "username": "Mark",
-        "firstName": "Mark",
-        "lastName": "Tven",
-        "email": "mark@mail.ru",
-        "password": "m533",
-        "phone": "89005335353",
-        "userStatus": 0
-      }
-      }).then((response) => {
-        expect(response.status).be.eql(200)
+// describe('zoo api test', () => {
+//   const baseUrl = `https://petstore.swagger.io/v2`;
+//   it('Создание пользователя', () => {
+//     cy.request({
+//       method: `POST`,
+//       url: `${baseUrl}/user`,
+//       body: {
+//         "id": 1,
+//         "username": "Mark",
+//         "firstName": "Mark",
+//         "lastName": "Tven",
+//         "email": "mark@mail.ru",
+//         "password": "m533",
+//         "phone": "89005335353",
+//         "userStatus": 0
+//       }
+//       }).then((response) => {
+//         expect(response.status).be.eql(200)
+//       })
+//     })
+    // cy.request({
+    //   method: `DELETE`,
+    //   url: `${baseUrl}/user`,
+    //   body: {
+    //     "id": 1,
+    //     "username": "Mark",
+    //     "firstName": "Mark",
+    //     "lastName": "Tven",
+    //     "email": "mark@mail.ru",
+    //     "password": "m533",
+    //     "phone": "89005335353",
+    //     "userStatus": 0
+    //   }
+    //  }).then((response) => {
+    //   expect(response.status).be.eql(200)
+    // })
+  // })
+
+import users from '../fixtures/users.json'
+
+describe(`swagger petstore`, () => {
+it(`create user`, () => {
+cy.createUser(users.user1)
+.then((response) => {
+expect(response.status).be.eql(200);
+
+    cy.getUser(users.user1.username, true)
+      .then((response) => {
+        expect(response.status).be.eql(200);
+        expect(response.body).be.eqls(users.user1);
+      })
+  })
+})
+it(`update user`, () => {
+cy.createUser(users.user2)
+.then((response) => {
+expect(response.status).be.eql(200);
+
+    cy.request('PUT', `/user/${users.user2.username}`, users.user3)
+      .then((response) => {
+        expect(response.status).be.eql(200);
+        cy.getUser(users.user3.username, true)
+        .then((response) => {
+          expect(response.status).be.eql(200);
+          expect(response.body).be.eqls(users.user3);
+        })
+      })
+  })
+})
+it(`delete user`, () => {
+cy.createUser(users.user4)
+.then((response) => {
+expect(response.status).be.eql(200);
+
+    cy.request('DELETE', `/user/${users.user4.username}`)
+      .then((response) => {
+        expect(response.status).be.eql(200);
+        cy.getUser(users.user4.username, false)
+        .then((response) => {
+          expect(response.status).be.eql(404);
+        })
       })
     })
-    cy.request({
-      method: `DELETE`,
-      url: `${baseUrl}/user`,
-      body: {
-        "id": 1,
-        "username": "Mark",
-        "firstName": "Mark",
-        "lastName": "Tven",
-        "email": "mark@mail.ru",
-        "password": "m533",
-        "phone": "89005335353",
-        "userStatus": 0
-      }
-     }).then((response) => {
-      expect(response.status).be.eql(200)
-    })
   })
+})
 
 
 
